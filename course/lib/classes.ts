1f91cc9039d1abdb3d4cc09d74a7a49c9b4c3420
duyenle1312 <lengomyduyen@gmail.com>
2024-11-23 18:01:55 +0200

    add OOP classes

--- course/lib/classes.ts
+++ course/lib/classes.ts
@@ -1,88 +1,227 @@
 import { removeStopwords } from "stopword";
+import { getCourses } from "./getRmd";
+import pool from "./db";
 
+type method_type = "matrix" | "cosine" | undefined;
 abstract class RecommendationSystem {
-    course_weights: Record<string, number> = {}
+  method: method_type = undefined;
 
-    topCourses(): Record<string, number> {
-        return this.course_weights
-    }
+  constructor(method: method_type) {
+    this.method = method;
+  }
 
-    abstract calculate(): void;
+  abstract calculate(): void;
 }
 
 export class RecommendationForUser extends RecommendationSystem {
-    user_id: number;
-    user_matrix: number[] = [];
-    course_matrix: number[][] = [];
+  user_id: number;
 
-    constructor(user_id: number) {
-        super()
-        this.user_id = user_id
-    }
 
-    calculate(): void {
-        console.log("RecommendationForUser")
-    }
+  constructor(user_id: number) {
+    super("matrix");
+    this.user_id = user_id;
+  }
+
+
+  async calculate() {
+    console.log("RecommendationForUser");
+
+    // Create course matrix
+    const topic_ids_query = await pool.query(
+      "SELECT topic_id FROM topics order by topic_id;"
+    );
+
+    const topic_ids: number[] = [];
+    topic_ids_query.rows.forEach((i: { topic_id: number }) =>
+      topic_ids.push(Number(i?.topic_id))
+    );
+
+    const copy_arr = (x: number) => x * 0;
+    const topic_values = topic_ids.map(copy_arr);
+
+    const course_ids_query = await pool.query(
+      "SELECT course_id, course_title FROM courses order by course_id;"
+    );
+
+    const course_ids: Record<number, number[]> = {};
+    const course_titles: string[] = [];
+    const courses: number[] = [];
+
+    course_ids_query.rows.forEach(
+      (i: { course_id: number; course_title: string }) => {
+        courses.push(Number(i?.course_id));
+        course_titles.push(i?.course_title);
+
+        const course = courses.indexOf(Number(i.course_id));
+        course_ids[course] = [...topic_values];
+      }
+    );
+
+    const course_topic_ids_query = await pool.query(
+      "SELECT course_id, topic_id FROM courses_topics order by course_id;"
+    );
+
+    course_topic_ids_query.rows.forEach(
+      (i: { course_id: number; topic_id: number }) => {
+        const course = courses.indexOf(Number(i.course_id));
+        const topic = topic_ids.indexOf(Number(i.topic_id));
+        course_ids[course][topic] = 1;
+      }
+    );
+
+    const course_keyword_matrix: number[][] = Object.values(course_ids);
+
+    // User matrix
+    const user_topic_ids_query = await pool.query(
+      "SELECT topic_id, ranking FROM users_topics where user_id = $1;",
+      [this.user_id]
+    );
+
+    const user_matrix = [...topic_values]; // initialize data
+    user_topic_ids_query.rows.forEach(
+      (i: { topic_id: number; ranking: number }) => {
+        const topic = Number(i.topic_id);
+        user_matrix[topic_ids.indexOf(topic)] = Number(i.ranking);
+      }
+    );
+
+    const matrix_product = (
+      course_matrix: number[][],
+      user_matrix: number[]
+    ) => {
+      const result = [];
+      if (course_matrix[0].length !== user_matrix.length) {
+        return [];
+      } else {
+        for (let row = 0; row < course_matrix.length; row++) {
+          let sum = 0;
+          for (let col = 0; col < course_matrix[row].length; col++) {
+            sum += course_matrix[row][col] * user_matrix[col];
+          }
+          result[row] = sum;
+        }
+        return result;
+      }
+    };
+
+    const result = matrix_product(course_keyword_matrix, user_matrix);
+
+    // Get the indexes of the top 10 courses with the highest value
+    const indexes = result
+      .map((value, index) => ({ value, index }))
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 10);
+
+    const recommendation: { id: string; title: string }[] = [];
+
+    indexes.forEach((i) => {
+      // result has the same length with courses and course_titles array, hence their index is the same
+      const course_id = courses[i.index].toString();
+      const course_title = course_titles[i.index];
+      recommendation.push({ id: course_id, title: course_title });
+    });
+
+    return recommendation;
+  }
 }
 
 export class RecommendationForCourse extends RecommendationSystem {
-    course_id: number;
+  course_id: number;
 
-    constructor(course_id: number) {
-        super()
-        this.course_id = course_id
+  constructor(course_id: number) {
+    super("cosine");
+    this.course_id = course_id;
+  }
+
+  tokenizer(text: string): string[] {
+    let tokens = text
+      .replace(/[^a-zA-Z ]/g, "")
+      .toLowerCase()
+      .split(" ");
+    tokens = removeStopwords(tokens).filter(function (i: string) {
+      return i != ""; // remove empty token
+    });
+    return tokens;
+  }
+
+  countWordFrequency(text: string): { [key: string]: number } {
+    const frequency: { [key: string]: number } = {};
+    const tokens = this.tokenizer(text);
+    tokens.forEach((i: string) => (frequency[i] = (frequency[i] || 0) + 1));
+    return frequency;
+  }
+
+  bagOfWords(tokens: string[], wordFreq: { [key: string]: number }): number[] {
+    const result: number[] = [];
+    tokens.forEach((t) => result.push(wordFreq[t] || 0));
+    return result;
+  }
+
+  // cosine similarity = (a · b) / (||a|| * ||b||)
+  cosineSimilarity(x: number[], y: number[]): number {
+    if (x.length !== y.length) {
+      console.log("Two vectors are not in the same length!");
+      return 0;
     }
 
-    tokenizer(text: string): string[] {
-        let tokens = text
-            .replace(/[^a-zA-Z ]/g, "")
-            .toLowerCase()
-            .split(" ");
-        tokens = removeStopwords(tokens).filter(function (i: string) {
-            return i != ""; // remove empty token
-        });
-        return tokens;
+    let product = 0;
+    let sum_x = 0;
+    let sum_y = 0;
+    for (let i = 0; i < x.length; i++) {
+      product += x[i] * y[i];
+      sum_x += x[i] ** 2;
+      sum_y += y[i] ** 2;
+    }
+    const result = product / (Math.sqrt(sum_x) * Math.sqrt(sum_y));
+    return result * 100;
+  }
 
+  async calculate() {
+    console.log("RecommendationForCourse");
+
+    const courses = await getCourses();
+    const course = courses.find((i) => i.id == this.course_id);
+    const potential_courses = courses.filter(
+      (i) => i.id != this.course_id
+    );
+
+    const recommendation = [];
+    // find course similarity
+    for (let i = 0; i < potential_courses.length; i++) {
+      // Tokenization
+      const course_info =
+        course?.course_title +
+        " " +
+        course?.what_you_will_learn +
+        " " +
+        course?.skill_gain;
+      const courseWordCount = this.countWordFrequency(course_info || "");
+
+      const target_info =
+        potential_courses[i].course_title +
+        " " +
+        potential_courses[i].what_you_will_learn +
+        " " +
+        potential_courses[i].skill_gain;
+
+      const targetWordCount = this.countWordFrequency(target_info || "");
+
+      const mergedText = course_info + " " + target_info;
+
+      let tokens: string[] = this.tokenizer(mergedText);
+      tokens = tokens.filter((i, index) => tokens.indexOf(i) === index); // remove duplicates
+
+      const courseVec = this.bagOfWords(tokens, courseWordCount);
+      const targetVec = this.bagOfWords(tokens, targetWordCount);
+
+      const similarity: number =
+        this.cosineSimilarity(courseVec, targetVec) || 0;
+
+      if (similarity > 50.0) {
+        recommendation.push(potential_courses[i]);
+      }
     }
 
-    countWordFrequency (text: string): { [key: string]: number } {
-        const frequency: { [key: string]: number } = {};
-        const tokens = this.tokenizer(text);
-        //console.log(tokens);
-        tokens.forEach((i: string) => (frequency[i] = (frequency[i] || 0) + 1));
-        //console.log(frequency);
-        return frequency;
-    };
-
-    bagOfWords(
-        tokens: string[],
-        wordFreq: { [key: string]: number }
-    ): number[] {
-        const result: number[] = [];
-        tokens.forEach((t) => result.push(wordFreq[t] || 0));
-        return result;
-    };
-
-    cosineSimilarity (x: number[], y: number[]): number {
-        if (x.length !== y.length) {
-            console.log("Two vectors are not in the same length!");
-            return 0;
-        }
-
-        let product = 0;
-        let sum_x = 0;
-        let sum_y = 0;
-        for (let i = 0; i < x.length; i++) {
-            product += x[i] * y[i];
-            sum_x += x[i] ** 2;
-            sum_y += y[i] ** 2;
-        }
-        const result = product / (Math.sqrt(sum_x) * Math.sqrt(sum_y));
-        return result * 100;
-    };
-
-    calculate(): void {
-        console.log("RecommendationForCourse")
-    }
+    return recommendation;
+  }
 }
-
--- course/lib/getRmd.ts
+++ course/lib/getRmd.ts
@@ -1,6 +1,7 @@
 import { removeStopwords } from "stopword";
 import { promises as fs } from "fs";
 import pool from "./db";
+import { RecommendationForCourse, RecommendationForUser } from "./classes";
 
 export type Course = {
   id: number;
@@ -84,109 +85,20 @@ export const findUserInterests = async (user_id: string) => {
   return user_interests.rows;
 };
 
+
 export const getRecommendationForUser = async (user_id: string) => {
-  // Create course matrix
-  const topic_ids_query = await pool.query(
-    "SELECT topic_id FROM topics order by topic_id;"
-  );
-
-  const topic_ids: number[] = [];
-  topic_ids_query.rows.forEach((i: { topic_id: number }) =>
-    topic_ids.push(Number(i?.topic_id))
-  );
-
-  const copy_arr = (x: number) => x * 0;
-  const topic_values = topic_ids.map(copy_arr);
-
-  const course_ids_query = await pool.query(
-    "SELECT course_id, course_title FROM courses order by course_id;"
-  );
-
-  const course_titles: string[] = [];
-  const course_ids: Record<number, number[]> = {};
-  const courses: number[] = [];
-
-  course_ids_query.rows.forEach(
-    (i: { course_id: number; course_title: string }) => {
-      courses.push(Number(i?.course_id));
-      course_titles.push(i?.course_title);
-
-      const course = courses.indexOf(Number(i.course_id));
-      course_ids[course] = [...topic_values];
-    }
-  );
-
-  const course_topic_ids_query = await pool.query(
-    "SELECT course_id, topic_id FROM courses_topics order by course_id;"
-  );
-
-  course_topic_ids_query.rows.forEach(
-    (i: { course_id: number; topic_id: number }) => {
-      const course = courses.indexOf(Number(i.course_id));
-      const topic = topic_ids.indexOf(Number(i.topic_id));
-      course_ids[course][topic] = 1;
-    }
-  );
-
-  const course_keyword_matrix: number[][] = Object.values(course_ids);
-
-  // User matrix
-  const user_topic_ids_query = await pool.query(
-    "SELECT topic_id, ranking FROM users_topics where user_id = $1;",
-    [user_id]
-  );
-
-  const user_matrix = [...topic_values]; // initialize data
-  user_topic_ids_query.rows.forEach(
-    (i: { topic_id: number; ranking: number }) => {
-      const topic = Number(i.topic_id);
-      user_matrix[topic_ids.indexOf(topic)] = Number(i.ranking);
-    }
-  );
-
-  const matrix_product = (course_matrix: number[][], user_matrix: number[]) => {
-    const result = [];
-    if (course_matrix[0].length !== user_matrix.length) {
-      return [];
-    } else {
-      for (let row = 0; row < course_matrix.length; row++) {
-        let sum = 0;
-        for (let col = 0; col < course_matrix[row].length; col++) {
-          sum += course_matrix[row][col] * user_matrix[col];
-        }
-        result[row] = sum;
-      }
-      return result;
-    }
-  };
-
-  const result = matrix_product(course_keyword_matrix, user_matrix);
-  
-  const course_weights: Record<number, number> = {}
-  
-  for (let i = 0; i < result.length; i++) {
-    course_weights[courses[i]] = result[i]
-  }
-  // console.log(course_weights)
-  
-
-  // Get the indexes of the top 10 courses with the highest value
-  const indexes = result
-    .map((value, index) => ({ value, index }))
-    .sort((a, b) => b.value - a.value)
-    .slice(0, 10);
-
-  const recommendation: { id: string; title: string }[] = [];
-
-  indexes.forEach((i) => {
-    // result has the same length with courses and course_titles array, hence their index is the same
-    const course_id = courses[i.index].toString();
-    const course_title = course_titles[i.index];
-    recommendation.push({ id: course_id, title: course_title });
-  });
-
-  return recommendation;
+  const instance = new RecommendationForUser(Number(user_id));
+  const recommendation = await instance.calculate();
+  return recommendation
 };
+
+
+export const getRecommendationForCourse = async (id: string) => {
+  const instance = new RecommendationForCourse(Number(id));
+  const recommendation = await instance.calculate();
+  return recommendation
+};
+
 
 export const populateCourseData = async () => {
   const file_name = "course_with_keywords.csv";
@@ -324,105 +236,4 @@ export const populateCourseData = async () => {
   }
 
   return new_data;
-};
-
-export const getRecommendationForCourse = async (id: string) => {
-  const courses = await getCourses();
-  const course = courses.find((i) => i.id.toString() === id);
-  const potential_courses = courses.filter(
-    (i) => i.id.toString() !== id // i.keyword === course_department &&
-  );
-
-  const tokenize = (text: string) => {
-    let tokens = text
-      .replace(/[^a-zA-Z ]/g, "")
-      .toLowerCase()
-      .split(" ");
-    tokens = removeStopwords(tokens).filter(function (i: string) {
-      return i != ""; // remove empty token
-    });
-    return tokens;
-  };
-
-  const countWordFrequency = (text: string) => {
-    const frequency: { [key: string]: number } = {};
-    const tokens = tokenize(text);
-    //console.log(tokens);
-    tokens.forEach((i) => (frequency[i] = (frequency[i] || 0) + 1));
-    //console.log(frequency);
-    return frequency;
-  };
-
-  const bagOfWords = (
-    tokens: string[],
-    wordFreq: { [key: string]: number }
-  ) => {
-    const result: number[] = [];
-    tokens.forEach((t) => result.push(wordFreq[t] || 0));
-    return result;
-  };
-
-  // cosine similarity = (a · b) / (||a|| * ||b||)
-  const cosine_similarity = (x: number[], y: number[]) => {
-    if (x.length !== y.length) {
-      console.log("Two vectors are not in the same length!");
-      return null;
-    }
-
-    let product = 0;
-    let sum_x = 0;
-    let sum_y = 0;
-    for (let i = 0; i < x.length; i++) {
-      product += x[i] * y[i];
-      sum_x += x[i] ** 2;
-      sum_y += y[i] ** 2;
-    }
-    const result = product / (Math.sqrt(sum_x) * Math.sqrt(sum_y));
-    return result * 100;
-  };
-
-  const recommendation = [];
-  const course_weights = []
-  // find course similarity
-  for (let i = 0; i < potential_courses.length; i++) {
-    // Tokenization
-    const course_info =
-      course?.course_title +
-      " " +
-      course?.what_you_will_learn +
-      " " +
-      course?.skill_gain;
-    const courseWordCount = countWordFrequency(course_info || "");
-    // console.log(courseWordCount);
-
-    const target_info =
-      potential_courses[i].course_title +
-      " " +
-      potential_courses[i].what_you_will_learn +
-      " " +
-      potential_courses[i].skill_gain;
-    const targetWordCount = countWordFrequency(target_info || "");
-    // console.log(targetWordCount);
-
-    const mergedText = course_info + " " + target_info;
-    // console.log(mergedText);
-
-    let tokens: string[] = tokenize(mergedText);
-    tokens = tokens.filter((i, index) => tokens.indexOf(i) === index); // remove duplicates
-    // console.log(tokens);
-
-    const courseVec = bagOfWords(tokens, courseWordCount);
-    // console.log(courseVec);
-    const targetVec = bagOfWords(tokens, targetWordCount);
-    // console.log(targetVec);
-
-    const similarity: number = cosine_similarity(courseVec, targetVec) || 0;
-    course_weights.push(similarity)
-
-    if (similarity > 50.0) {
-      recommendation.push(potential_courses[i]);
-    }
-  }
-
-  return recommendation;
 };
